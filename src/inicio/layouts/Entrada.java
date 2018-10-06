4d2c610e892231afb154b0f9e26ec4abd1d75586
Develop <info@developandsys.es>
2018-10-06 15:10:30 +0200

    introduccion swing

--- src/inicio/layouts/Entrada.java
+++ src/inicio/layouts/Entrada.java
@@ -2,13 +2,14 @@ package inicio.layouts;
 
 import javax.swing.*;
 
-public class EntradaCard {
+public class Entrada {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 //VentanaCard v = new VentanaCard();
+                VentanaGridFlowBorder v = new VentanaGridFlowBorder();
             }
         });
     }
---
+++ src/inicio/layouts/VentanaGridFlowBorder.java
@@ -0,0 +1,84 @@
+package inicio.layouts;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.SwingConstants.CENTER;
+
+public class VentanaGridFlowBorder extends JFrame {
+
+    JPanel pCentra, pIzquierda, pDerecha;
+    Container container;
+
+    public VentanaGridFlowBorder() throws HeadlessException {
+        instancias();
+        initGUI();
+
+    }
+
+    private void instancias() {
+        pCentra = new JPanel();
+        pDerecha = new JPanel();
+        pIzquierda= new JPanel();
+        container = getContentPane();
+    }
+
+    private void initGUI() {
+
+        container.add(configurarCentro(),BorderLayout.CENTER);
+        container.add(configurarArriba(),BorderLayout.NORTH);
+        container.add(configurarDerecha(),BorderLayout.EAST);
+        container.add(configuraIzquierda(),BorderLayout.WEST);
+        container.add(configurarAbajo(),BorderLayout.SOUTH);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setTitle("Layouts Grid Border y Flow");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setExtendedState(MAXIMIZED_BOTH);
+    }
+
+    private JComponent configurarCentro() {
+
+        JLabel label = new JLabel("Etiqueta centro");
+        label.setHorizontalAlignment(CENTER);
+        return label;
+    }
+    private JComponent configurarArriba() {
+
+        JLabel label = new JLabel("Etiqueta arriba");
+        label.setHorizontalAlignment(CENTER);
+        return label;
+    }
+    private JComponent configurarDerecha() {
+
+        JButton b1 = new JButton("Boton d1");
+        JButton b2 = new JButton("Boton d2");
+        JButton b3 = new JButton("Boton d3");
+        JButton b4 = new JButton("Boton d4");
+        pDerecha.setLayout(new GridLayout(4,1,10,0));
+        pDerecha.add(b1);
+        pDerecha.add(b2);
+        pDerecha.add(b3);
+        pDerecha.add(b4);
+        return pDerecha;
+    }
+    private JComponent configuraIzquierda() {
+
+        JButton b1 = new JButton("Boton i1");
+        JButton b2 = new JButton("Boton i2");
+        JButton b3 = new JButton("Boton i3");
+        JButton b4 = new JButton("Boton i4");
+        pIzquierda.setLayout(new GridLayout(4,1,10,0));
+        pIzquierda.add(b1);
+        pIzquierda.add(b2);
+        pIzquierda.add(b3);
+        pIzquierda.add(b4);
+        return pIzquierda;
+    }
+    private JComponent configurarAbajo() {
+
+        JLabel label = new JLabel("Etiqueta abajo");
+        label.setHorizontalAlignment(CENTER);
+        return label;
+    }
+}
